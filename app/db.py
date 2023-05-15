33bc49e39c21b1ba71657d943166dc6e16d5b537
h4sski <h4sski.programming.gmail.com>
2023-05-15 22:22:25 +0200

    update

--- app/db.py
+++ app/db.py
@@ -1,6 +1,9 @@
 import sqlite3
 from flask import current_app, g
 
+
+DB_NAME = 'database.db'
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -8,11 +11,11 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-    
+
     return g.db
 
 
-def close_db():
+def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
@@ -24,3 +27,22 @@ def with_db_open(inner_function, *args, **kwargs):
     result = inner_function(db=database, *args, **kwargs)
     close_db()
     return result
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+        
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
---
+++ app/models.py
@@ -0,0 +1,7 @@
+from os import path
+
+import sqlite3
+from flask import g
+
+
+DB_NAME = 'database.db'
